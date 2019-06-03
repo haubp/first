c102bfcf6d0a4b0ed35eb06035ba3455705a3649
haubp <haubp>
2019-06-03 03:05:40 -0700

    haubp update

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var multer  = require('multer');
+
+var upload = multer({ dest: 'upload/'});
 var app = express();
 
+var type = upload.single('recfile');
+
+app.use(express.static('public'));
+
+/* Status indicates that new firmware need to update */
 update = 'false'
 
 app.get('/status', function(req, res) {
@@ -9,22 +18,24 @@ app.get('/status', function(req, res) {
     res.send(status);
 });
 
-app.post('/pushFirmware', function(req, res) {
+app.post('/pushFirmware', type, function(req, res) {
     update = 'true';
 
-    // Save new firmware
+    var tmp_path = req.file.path;
+    var target_path = '/uploads/' + req.file.originalname;
 
-
-    res.send('saved new firmware');
-});
+    var src = fs.createReadStream(tmp_path);
+    var dest = fs.createWriteStream(target_path);
+    src.pipe(dest);
+    src.on('end', function() { res.send('complete'); });
+    src.on('error', function(err) { res.send('error'); });
+})
 
 app.get('/pushFirmwarePage', function(req, res) {
     res.sendFile(__dirname + "/" + "views/index.html");
 });
 
-app.use(express.static('public'));
-
-var server = app.listen(process.env.PORT, function() {
+var server = app.listen(/*process.env.PORT*/5000, function() {
     var host = server.address().address;
     var port = server.address().port;
 
