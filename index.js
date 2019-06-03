59fefb183b1a28116e53139b9cbfef68aad5c770
haubp <haubp>
2019-06-03 00:28:42 -0700

    expr

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('Hello World'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// doUpdate = "false"
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.send(doUpdate))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hi there');
+})
+
+var server = app.listen(process.env.PORT, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+})
