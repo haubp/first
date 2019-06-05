a81e998fb5c2e52930247f72fee7142aafbc905d
haubp <haubp>
2019-06-04 19:04:59 -0700

    haubp update

--- index.js
+++ index.js
@@ -10,19 +10,39 @@ var type = upload.single('myfile');
 app.use(express.static('public'));
 
 /* Status indicates that new firmware need to update */
-update = 'false'
+update = 'alluptodate'
 
 app.get('/status', function(req, res) {
     status = update;
-    update = 'false';
+    update = 'alluptodate';
     res.send(status);
 });
 
 app.post('/pushFirmware', type, function(req, res) {
-    update = 'true';
 
     var tmp_path = req.file.path;
     var target_path = './public/firmware/firmware.bin';
+
+    console.log(req.file.originalname);
+    switch(req.file.originalname)
+    {
+        case 'ecu_1_ver1.bin':
+        case 'ecu_1_ver2.bin':
+            update = 'ecu1';
+            break;
+        case 'ecu_2_ver1.bin':
+        case 'ecu_2_ver2.bin':
+            update = 'ecu2';
+            break;
+        case 'ecu_3_ver1.bin':
+        case 'ecu_3_ver2.bin':
+            update = 'ecu3';
+            break;
+        case 'ecu_4_ver1.bin':
+        case 'ecu_4_ver2.bin':
+            update = 'ecu4';
+            break;
+    }
 
     var src = fs.createReadStream(tmp_path);
     var dest = fs.createWriteStream(target_path);
